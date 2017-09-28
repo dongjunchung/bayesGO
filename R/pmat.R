dfe8a1230674459ad5c99fe3a9e4bad9b6db097e
Dongjun Chung <dongjun.chung@gmail.com>
2017-09-28 09:08:21 -0400

    Real datasets are included

---
+++ R/pmat.R
@@ -0,0 +1,14 @@
+#' @title real data for human genes
+#'
+#' @description real data for human genes
+#' @name pmat
+#' @docType data
+#' @usage pmat
+#' @format A matrix of hypergeometric p-values, where rows and columns correspond to GO terms and genes.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(pmat)
+
+NULL
---
+++ R/yeast.R
@@ -0,0 +1,14 @@
+#' @title real data for yeast genes
+#'
+#' @description real data for yeast genes
+#' @name yeast
+#' @docType data
+#' @usage yeast
+#' @format A matrix of hypergeometric p-values, where rows and columns correspond to GO terms and genes.
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(yeast)
+
+NULL
